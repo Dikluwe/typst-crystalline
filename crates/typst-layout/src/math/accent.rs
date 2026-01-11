mkorje/typst

Use `Engine` in place of `MathContext` in some functions

crates/typst-layout/src/math/accent.rs
@@ -53,7 +53,7 @@ pub fn layout_accent(
     // in many cases.
     let width = elem.size.resolve(styles).relative_to(base.width());
     let short_fall = ACCENT_SHORT_FALL.at(size);
-    accent.stretch_horizontal(ctx, width, short_fall);
+    accent.stretch_horizontal(ctx.engine, width, short_fall);
     let accent_attach = accent.accent_attach().0;
     let accent = accent.into_frame();
 

Dikluwe/typst-crystalline

crates/typst-layout/src/math/accent.rs
@@ -103,7 +103,7 @@ pub fn place_accent(
     // in many cases.
     let stretch_width = stretch_width.relative_to(base.width());
     let short_fall = short_fall.at(size);
-    accent.stretch_horizontal(ctx, stretch_width, short_fall);
+    accent.stretch_horizontal(ctx.engine, stretch_width, short_fall);
     let accent_attach = accent.accent_attach().0;
     let accent = accent.into_frame();
 